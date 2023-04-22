1061be4f7e8c36a1781ebb3e5c8a284a82743be6
miguelgargallo <mgargallo16@gmail.com>
2023-04-22 13:24:24 +0200

    gh 0.0.5-1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
-import { resolve } from 'pathe'
+import { resolve } from "pathe";
 // import consola from 'consola'
 
-const alias = {}
+const alias = {};
 
 // if (process.env.NODE_ENV === 'development') {
 //   consola.warn('Using local @nuxt/content!')
@@ -15,76 +15,77 @@ export default defineNuxtConfig({
       script: [
         {
           defer: true,
-          'data-domain': 'content.nuxtjs.org',
-          src: 'https://plausible.io/js/script.js'
-        }
-      ]
-    }
+          "data-domain": "content.nuxtjs.org",
+          src: "https://plausible.io/js/script.js",
+        },
+      ],
+    },
   },
   content: {
     sources: {
       v1: {
-        prefix: '/v1',
-        driver: 'fs',
-        base: resolve(__dirname, 'content-v1/en')
+        prefix: "/v1",
+        driver: "fs",
+        base: resolve(__dirname, "content-v1/en"),
       },
-      'v1-ja': {
-        prefix: '/ja/v1',
-        driver: 'fs',
-        base: resolve(__dirname, 'content-v1/ja')
+      "v1-ja": {
+        prefix: "/ja/v1",
+        driver: "fs",
+        base: resolve(__dirname, "content-v1/ja"),
       },
-      'v1-fr': {
-        prefix: '/fr/v1',
-        driver: 'fs',
-        base: resolve(__dirname, 'content-v1/fr')
+      "v1-fr": {
+        prefix: "/fr/v1",
+        driver: "fs",
+        base: resolve(__dirname, "content-v1/fr"),
       },
-      'v1-ru': {
-        prefix: '/ru/v1',
-        driver: 'fs',
-        base: resolve(__dirname, 'content-v1/ru')
-      }
+      "v1-ru": {
+        prefix: "/ru/v1",
+        driver: "fs",
+        base: resolve(__dirname, "content-v1/ru"),
+      },
     },
     highlight: {
-      preload: ['xml']
-    }
+      preload: ["xml"],
+    },
   },
   nitro: {
     prerender: {
       routes: [
-        '/',
-        '/blog/announcing-v2',
+        "/",
+        "/blog/announcing-v2",
         // TODO: move to github module
-        '/api/_github/releases/.json'
-      ]
-    }
+        "/api/_github/releases/.json",
+      ],
+    },
   },
-  modules: ['@nuxtlabs/github-module', '@nuxthq/studio'],
-  extends: process.env.DOCUS_THEME_PATH || '@nuxt-themes/docus',
+  modules: ["@nuxtlabs/github-module", "@nuxthq/studio", "nuxt-meilisearch"],
+  extends: process.env.DOCUS_THEME_PATH || "@nuxt-themes/docus",
   github: {
-    owner: 'nuxt',
-    repo: 'content',
-    branch: 'main'
+    owner: "nuxt",
+    repo: "content",
+    branch: "main",
   },
   colorMode: {
-    preference: 'dark'
+    preference: "dark",
   },
   runtimeConfig: {
     content: {
       // @ts-ignore
       // TODO: fix types
       documentDriven: {
-        host: 'https://content.nuxtjs.org'
-      }
+        host: "https://content.nuxtjs.org",
+      },
     },
     public: {
-      algolia: {
-        applicationId: 'E3TJT7KISI',
-        apiKey: 'f313571f5d16bd5f2e52423edd4a49ba',
-        langAttribute: 'lang',
-        docSearch: {
-          indexName: 'content-nuxtjs'
-        }
-      }
-    }
-  }
-})
+      meilisearch: {
+        hostUrl: "http://my-meilisearch-server.domain.com",
+        searchApiKey: "<your_public_key>",
+        adminApiKey: "<your_secret_key>",
+        serverSideUsage: true,
+        instantSearch: {
+          theme: "algolia",
+        },
+      },
+    },
+  },
+});
